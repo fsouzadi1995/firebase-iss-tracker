1e1947e785b7f0b03908979a868b49421da11d6e
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-05-12 00:16:25 -0300

    chore: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.html',
+  output: {
+    filename: 'bundle.html',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};

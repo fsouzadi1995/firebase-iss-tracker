6b0f13424f43e612c337a764a099dc03474b3c3f
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-05-22 19:23:20 -0300

    chore: another try at netlify

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import './style.css';
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGE_SENDER_ID,
-  appId: APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const fbase = firebase.initializeApp(firebaseConfig);
